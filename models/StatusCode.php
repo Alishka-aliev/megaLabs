559ed68f729e88d5e82e1c2d7f01d7be1ca9a947
ali aliev <a.aliev.01@gmail.com>
2017-11-01 01:23:17 +0300

    Развернул ghjtrn

---
+++ models/StatusCode.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ali
+ * Date: 01.11.17
+ * Time: 1:03
+ */
+namespace app\models;
+
+class StatusCode {
+
+
+
+//https://msdn.microsoft.com/en-us/library/system.net.httpstatuscode(v=vs.110).aspx
+    public function status($typeCode) {
+       switch ($typeCode) {
+           case 200 : return true; break;
+           case 302 : return true; break;
+           default : return false; break;
+       }
+    }
+}
